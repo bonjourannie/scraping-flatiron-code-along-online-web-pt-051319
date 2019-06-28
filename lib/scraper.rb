8e111fb462df72dc6542b2a572a7762a1f298275
Annie Truex <artruex@gmail.com>
2019-06-28 18:04:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,6 +11,12 @@ class Scraper
     #binding.pry
   end
   
+  def get_courses 
+  end
+  
+  def make_courses
+  end
+  
     def print_courses
     self.make_courses
     Course.all.each do |course|
